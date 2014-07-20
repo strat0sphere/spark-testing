d264464c2fc85c0cedb971b69a47576e026fc0eb
Stratos Dimopoulos <stratos@sphere.local>
2014-07-20 13:43:20 -0700

    change java dir name to be compatible with tachyon naming

--- prepare-slaves-ubuntu.sh
+++ prepare-slaves-ubuntu.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get --yes --force-yes install git
 apt-get --yes --force-yes install openjdk-7-jdk
 apt-get --yes --force-yes install scala
+mv /usr/lib/jvm/java-7-openjdk-amd64/ /usr/lib/jvm/java-1.7.0/
 #echo 'export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.65.x86_64/' >> ~/.bash_profile
 #echo 'export SCALA_HOME=/usr/share/java/' >> ~/.bash_profile
 #cp /home/ubuntu/.bash_profile /root/.bash_profile
