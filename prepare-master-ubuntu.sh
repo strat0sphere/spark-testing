85ec3d91d0f7cee2be57f44407c2a4b490cf6387
Stratos Dimopoulos <stratos@sphere.local>
2014-07-19 19:20:03 -0700

    bug fixes

--- prepare-master-ubuntu.sh
+++ prepare-master-ubuntu.sh
@@ -10,19 +10,13 @@ done
 wait
 
 #installing required packages to slave nodes
-echo Installing required packages to slave nodes..."
+echo "Installing required packages to slave nodes..."
 for node in $SLAVES $OTHER_MASTERS; do
   echo $node
   ssh -t -t $SSH_OPTS root@$node "spark-testing/prepare-slave.sh" & sleep 0.3
-  ssh -t -t $SSH_OPTS root@$node "chmod u+x spark_testing/prepare-slave.sh" & sleep 0.3
+  ssh -t -t $SSH_OPTS root@$node "chmod u+x spark-testing/prepare-slave.sh" & sleep 0.3
+  ssh -t -t $SSH_OPTS root@$node "echo 'export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.65.x86_64/' >> ~/.bash_profile"
+  ssh -t -t $SSH_OPTS root@$node "echo 'export SCALA_HOME=/usr/share/java/' >> ~/.bash_profile"
+  ssh -t -t $SSH_OPTS root@$node "source ~/.bash_profile"
 done
 wait
-apt-get update
-apt-get --yes --force-yes install wget
-apt-get --yes --force-yes install git
-apt-get --yes --force-yes install openjdk-7-jdk
-apt-get --yes --force-yes install scala
-echo 'export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.65.x86_64/' >> ~/.bash_profile
-echo 'export SCALA_HOME=/usr/share/java/' >> ~/.bash_profile
-cp /home/ubuntu/.bash_profile /root/.bash_profile
-source /root/.bash_profile
